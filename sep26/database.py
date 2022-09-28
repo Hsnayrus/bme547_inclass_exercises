e13d3ee9218b4d9ab62f60d8cf4881fac9b6b050
hsnayrus <suryanshjain.1309@gmail.com>
2022-09-28 12:07:18 -0400

    Created a class for a Patient and added init methods to help with initialization

--- sep26/database.py
+++ sep26/database.py
@@ -1,6 +1,22 @@
+class Patient:
+
+    def __init__(self, first_name, last_name, id, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.patient_id = id
+        self.age = age
+        self.tests = []
+
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
 def create_patient_entry(patient_first_name, patient_last_name, patient_id, patient_age):
-    new_patient = {"First Name": patient_first_name, "Last Name": patient_last_name,
-                   "Id": patient_id, "Age": patient_age, "Tests": []}
+    new_patient = Patient()
+    new_patient.first_name = patient_first_name
+    new_patient.last_name = patient_last_name
+    new_patient.patient_id = patient_id
+    new_patient.age = patient_age
     return new_patient
 
 
@@ -35,6 +51,13 @@ def get_full_name(patient):
 
 
 def main():
+    x = Patient("Suryansh", "Jain", "", "")
+    x.first_name = "Suryansh"
+    x.last_name = "Jain"
+    print(x.full_name())
+    print(type(x))
+    exit()
+
     db = {}
     db[11] = create_patient_entry("Ann", "Ables", 11, 30)
     db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
